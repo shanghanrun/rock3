5c5955b39461fdac9400ecd305d6e1203e32d5ae
shanghanrun <idim7@naver.com>
2024-03-19 13:33:16 +0900

    완성

--- src/App.js
+++ src/App.js
@@ -9,17 +9,54 @@ function App() {
   const smallImages=['/img/r1.png', '/img/s1.png', '/img/p1.png']
   const bigImages =['/img/rock.png', '/img/scissors.png', 'img/paper.png']
 
-  const [user, setUser] =useState({name:'rock' ,image:'/img/rock.png',status:'normal' })
-  const [computer, setComputer] =useState({name:'scissors' ,image:'/img/scissors.png',status:'normal' })
+  const [user, setUser] =useState(null)
+  // {name:'rock', image:'', status:'Win'}
+  const [computer, setComputer] =useState(null)
   const [result, setResult] = useState('Tie')
   const [score, setScore] =useState({user:0, computer:0})
+
+  function getComputer(){
+    const index = Math.floor(Math.random()*types.length)
+    const type = types[index]
+    const image = bigImages[index]
+    return [type, image];
+  }
+  function judgement(userChoice, computerChoice){
+    if(userChoice == computerChoice){
+      return 'Tie' 
+    } else if (userChoice=='rock'){
+      return computerChoice == 'scissors'? 'Win':'Lose'
+    } else if (userChoice =='scissors'){
+      return computerChoice =='rock'?"Lose":"Win"
+    } else if(userChoice =='paper'){
+      return computerChoice=='rock'? "Win":"Lose"
+    }
+  }
   function play(type){
+    const userChoice = type;
     let index;
     if(type==='rock'){index=0 }
     else if(type ==='scissors'){index=1}
     else {index=2}
     console.log(`${type}, ${index} 클릭됨`)
-    setUser({...user, name:type, image:bigImages[index]})
+    const [computerChoice, computerImage] = getComputer()
+    let result = judgement(userChoice, computerChoice)
+    if (result== 'Win'){
+      setScore({...score, user: score.user+1})
+      setUser({...user, name:type, image:bigImages[index],status:'Win'})
+      setComputer({...computer,name:computerChoice, image:computerImage, status:"Lose"})
+      setResult(result)
+    } else if(result =='Lose'){
+      setScore({...score, computer: score.computer+1})
+      setUser({...user, name:type, image:bigImages[index],status:'Lose'})
+      setComputer({...computer, name:computerChoice, image:computerImage,status:"Win"})
+      setResult(result)
+    } else{
+      setUser({...user, name:type, image:bigImages[index],status:'Tie'})
+      setComputer({...computer, name:computerChoice, image:computerImage,status:"Tie"})
+      setResult(result)
+    }
+    
   }
   return (
     <div>
--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Box = ({who}) => {
   return (
-	<div className={who.status}>
-		<img src={who.image} width="500px" height="500px" alt='' />
-		<div>{who.name}</div>
+	<div className={who?.status}>
+		<img src={who?.image} width="500px" height="500px" alt='' />
+		<div>{who?.name}</div>
 	</div>
   )
 }
